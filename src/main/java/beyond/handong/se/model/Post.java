f01d36f8b8fffa59bcbac9570f9170bc6f7ef936
21600212 <21600212@handong.edu>
2021-05-07 20:43:04 +0900

    Post Object related files - JPA with AWS RDS success

--- src/main/java/beyond/handong/se/model/Post.java
+++ src/main/java/beyond/handong/se/model/Post.java
@@ -1,52 +1,34 @@
 package beyond.handong.se.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.Date;
 
+@Entity
 public class Post {
 
-    /** Post 는 게시글 작성 관련해서 DB에 담을 정보들을 정의한 model 이다.
-     *  1. 글 번호 -> id| if type is not "공지" or "FAQ", print id
-     *  2. 제목 -> title
-     *  3. 작성자 -> writer
-     *  4. 작성 일시 -> writeDate
-     *  5. 수정 일시 -> lastEditDate
-     *  6. 내용 -> content
-     *  7. 고마워요숫자 -> helpfulNum
-     *  8. 스크랩수 -> scrapNum
-     *  9. 조회수 -> viewNum
-     *
-     *  TODO : check if it is enough to construct the table -Jerome
-     * */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long writerId;
+    private String writerName;
 
-    private Long id;        // primary key
-    private String writer;
     private String type;
     private String title;
     private String content;
+
+    private String country;
+    private String category;
+
     private Long helpfulNum;
     private Long scrapNum;
     private Long viewNum;
+
     private Date writeDate;
     private Date lastEditDate;
-
-    // Multiple variables in a single post object
-    private ArrayList<Long> imgId;          // PK of PostImage, Post_ID is FK
-    private ArrayList<Long> countryId;      // PK of Country, Post_ID is FK
-    private ArrayList<Long> categoryId;     // PK of Category, Post_ID is FK
-    private ArrayList<Long> commentId;      // PK of Comment, Post_ID is FK
-    private ArrayList<Long> scrapListId;    // PK of MyScrapList, Post_ID is FK
-    private ArrayList<Long> commentOrder;
-    private ArrayList<Long> commentLayer;
-    private ArrayList<Long> commentGroup;
-
-
-    private ArrayList<String> imgPath;
-    private ArrayList<String> country;
-    private ArrayList<String> category;
-    private ArrayList<String> comment;
-    private ArrayList<String> commentWriteDate;
-    private ArrayList<String> commentWriter;
 
     public Long getId() {
         return id;
@@ -56,12 +38,12 @@ public class Post {
         this.id = id;
     }
 
-    public String getWriter() {
-        return writer;
+    public Long getWriterId() {
+        return writerId;
     }
 
-    public void setWriter(String writer) {
-        this.writer = writer;
+    public void setWriterId(Long writerId) {
+        this.writerId = writerId;
     }
 
     public String getType() {
@@ -128,115 +110,27 @@ public class Post {
         this.lastEditDate = lastEditDate;
     }
 
-    public ArrayList<Long> getImgId() {
-        return imgId;
+    public String getWriterName() {
+        return writerName;
     }
 
-    public void setImgId(ArrayList<Long> imgId) {
-        this.imgId = imgId;
+    public void setWriterName(String writerName) {
+        this.writerName = writerName;
     }
 
-    public ArrayList<Long> getCountryId() {
-        return countryId;
-    }
-
-    public void setCountryId(ArrayList<Long> countryId) {
-        this.countryId = countryId;
-    }
-
-    public ArrayList<Long> getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(ArrayList<Long> categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public ArrayList<Long> getCommentId() {
-        return commentId;
-    }
-
-    public void setCommentId(ArrayList<Long> commentId) {
-        this.commentId = commentId;
-    }
-
-    public ArrayList<Long> getScrapListId() {
-        return scrapListId;
-    }
-
-    public void setScrapListId(ArrayList<Long> scrapListId) {
-        this.scrapListId = scrapListId;
-    }
-
-    public ArrayList<Long> getCommentOrder() {
-        return commentOrder;
-    }
-
-    public void setCommentOrder(ArrayList<Long> commentOrder) {
-        this.commentOrder = commentOrder;
-    }
-
-    public ArrayList<Long> getCommentLayer() {
-        return commentLayer;
-    }
-
-    public void setCommentLayer(ArrayList<Long> commentLayer) {
-        this.commentLayer = commentLayer;
-    }
-
-    public ArrayList<Long> getCommentGroup() {
-        return commentGroup;
-    }
-
-    public void setCommentGroup(ArrayList<Long> commentGroup) {
-        this.commentGroup = commentGroup;
-    }
-
-    public ArrayList<String> getImgPath() {
-        return imgPath;
-    }
-
-    public void setImgPath(ArrayList<String> imgPath) {
-        this.imgPath = imgPath;
-    }
-
-    public ArrayList<String> getCountry() {
+    public String getCountry() {
         return country;
     }
 
-    public void setCountry(ArrayList<String> country) {
+    public void setCountry(String country) {
         this.country = country;
     }
 
-    public ArrayList<String> getCategory() {
+    public String getCategory() {
         return category;
     }
 
-    public void setCategory(ArrayList<String> category) {
+    public void setCategory(String category) {
         this.category = category;
-    }
-
-    public ArrayList<String> getComment() {
-        return comment;
-    }
-
-    public void setComment(ArrayList<String> comment) {
-        this.comment = comment;
-    }
-
-    public ArrayList<String> getCommentWriteDate() {
-        return commentWriteDate;
-    }
-
-    public void setCommentWriteDate(ArrayList<String> commentWriteDate) {
-        this.commentWriteDate = commentWriteDate;
-    }
-
-    public ArrayList<String> getCommentWriter() {
-        return commentWriter;
-    }
-
-    public void setCommentWriter(ArrayList<String> commentWriter) {
-        this.commentWriter = commentWriter;
     }
 }
