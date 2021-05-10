696d091b7ad48e5c7f37a9dc6a12e7bfd171bea7
21600212 <21600212@handong.edu>
2021-05-10 23:42:41 +0900

    get Post API with multiple filters

--- src/test/java/beyond/handong/se/service/PostServiceIntegrationTest.java
+++ src/test/java/beyond/handong/se/service/PostServiceIntegrationTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -22,11 +24,19 @@ class PostServiceIntegrationTest {
     // 테스트 코드는 과감하게 한글로 적어도 된다.
     // 테스트 코드는 빌드될 때 포함되지 않는다.
     @Test
-    void 회원가입() {
+    void 새글작성() {
         // given
         Post post = new Post();
         post.setTitle("test");
         post.setWriterId(2L);
+
+        post.setHelpfulNum(0l);
+        post.setScrapNum(0l);
+        post.setViewNum(0l);
+
+        Date writeDate = new Date();
+        post.setWriteDate(writeDate);
+        post.setWriteDate(writeDate);
 
         // when
         Long saveId = postService.join(post);
@@ -34,32 +44,6 @@ class PostServiceIntegrationTest {
         // then
         Post findPost = postService.findOne(saveId).get();
         assertThat(post.getTitle()).isEqualTo(findPost.getTitle());
-    }
-
-    @Test
-    public void 중복_회원_예외() {
-        // given
-        Post post1 = new Post();
-        post1.setTitle("test");
-        post1.setWriterId(2L);
-
-        Post post2 = new Post();
-        post2.setTitle("test");
-        post2.setWriterId(2L);
-
-        // when
-        postService.join(post1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> postService.join(post2));
-
-//        try {
-//            PostService.join(Post2);
-//            fail();
-//        } catch (IllegalStateException e) {
-//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-//        }
-
-        // then
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 
     @Test
