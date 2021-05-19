8b3c590d06b2eb846ebef2c894f3cec6a2e92527
21600212 <21600212@handong.edu>
2021-05-19 15:12:57 +0900

    add comment model, controller, service

---
+++ src/main/java/beyond/handong/se/controller/CommentController.java
@@ -0,0 +1,40 @@
+package beyond.handong.se.controller;
+
+import beyond.handong.se.model.Comment;
+import beyond.handong.se.model.Post;
+import beyond.handong.se.service.CommentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class CommentController {
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping("comment/{post_id}")
+    @ResponseBody
+    public List<Comment> allPosts(@PathVariable("post_id") Long post_id) {
+        return commentService.findCommentsByPostId(post_id);
+    }
+
+    @PostMapping("comment")
+    @ResponseBody
+    public Long saveComment(Post post, Comment comment) {
+        comment.setPost_id(post.getId());
+        return commentService.saveNewComment(comment);
+    }
+
+    @PutMapping("comment")
+    @ResponseBody
+    public void editComment(Comment comment) {
+        commentService.editComment(comment);
+    }
+}
---
+++ src/main/java/beyond/handong/se/service/CommentService.java
@@ -0,0 +1,39 @@
+package beyond.handong.se.service;
+
+import beyond.handong.se.model.Comment;
+import beyond.handong.se.repository.CommentRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Transactional
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    EntityManager em;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> findCommentsByPostId(Long post_id){
+        return commentRepository.findCommentsByPost_Id(post_id);
+    }
+
+    public Long saveNewComment(Comment comment){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        String writeDate = format.format(new Date());
+
+        comment.setWriteDate(writeDate);
+
+        commentRepository.save(comment);
+        return comment.getId();
+    }
+
+    public void editComment(Comment comment){
+        commentRepository.save(comment);
+    }
+}
